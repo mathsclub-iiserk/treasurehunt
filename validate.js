e0a12e58669348c86b159f44e70ee449d6aa9696
Satvik Saha <sahasatvik@gmail.com>
2021-02-19 19:01:41 +0530

    Tweaked hashing setup

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 var solutions = [
-    "c90d5ac2ba58bf1fc1da7053d68e43451019bb1a8937c9dde9234f85b3ab6145",
-    "f2ba95ef8d61c5b1ab850d3d3ed904ae945cc02442f26e72d22a95e64c583014",
+    "120006163b49009308eb2ec2fae9e7d5543e8ba034d52df6e05d44c2f2eedabc",
+    "ef7294be0808a151090eca0461e41fd449390ad3153358ba374ed80007569e76",
 ];
 
 var input = document.getElementById("answer");
@@ -11,19 +11,20 @@ input.addEventListener("keyup", function(event) {
     }
 });
 
+var title = document.getElementsByClassName("title-box")[0].innerHTML.trim();
+
 function validate() {
     var answer = input.value;
     answer = answer.trim().toLowerCase();
     if (answer.length == 0) {
         return;
     }
-    var title = document.title;
-    var hash = sha256(title + answer);
-    var saltedHash = sha256(title + answer + "salt");
-    // console.log(hash);
-    // console.log(saltedHash);
-    if (solutions.includes(saltedHash)) {
-        window.location.href = "../" + hash + "/";
+    var hash1 = sha256(title + answer);
+    var hash2 = sha256(title + answer + title);
+    // console.log(hash1);
+    // console.log(hash2);
+    if (solutions.includes(hash2)) {
+        window.location.href = "../" + hash1 + "/";
     } else {
         input.classList.add("retry");
     }
