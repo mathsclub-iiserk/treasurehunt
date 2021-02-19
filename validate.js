572f1a980aed6a876a88d452bcedd64ab84bfe8e
Satvik Saha <sahasatvik@gmail.com>
2021-02-19 12:41:11 +0530

    Swapped SHA1 for SHA256

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 var solutions = [
-    "9cced6a5cc110525ab52e7e79857af4ab5531247",
-    "906604aebce38f9dfafaf0ad8e921a00773bee94",
+    "c90d5ac2ba58bf1fc1da7053d68e43451019bb1a8937c9dde9234f85b3ab6145",
+    "f2ba95ef8d61c5b1ab850d3d3ed904ae945cc02442f26e72d22a95e64c583014",
 ];
 
 var input = document.getElementById("answer");
@@ -18,10 +18,10 @@ function validate() {
         return;
     }
     var title = document.title;
-    var hash = sha1(title + answer);
-    var saltedHash = sha1(title + answer + "salt");
-    console.log(hash);
-    console.log(saltedHash);
+    var hash = sha256(title + answer);
+    var saltedHash = sha256(title + answer + "salt");
+    // console.log(hash);
+    // console.log(saltedHash);
     if (solutions.includes(saltedHash)) {
         window.location.href = "../" + hash + "/";
     } else {
