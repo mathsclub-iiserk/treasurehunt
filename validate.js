ce69c897e1229b5b6e1ed5eb2610f6630bdeaa0a
Satvik Saha <sahasatvik@gmail.com>
2021-02-23 23:07:02 +0530

    Added few more puzzles

--- validate.js
+++ validate.js
@@ -1,6 +1,8 @@
 var solutions = [
     "120006163b49009308eb2ec2fae9e7d5543e8ba034d52df6e05d44c2f2eedabc",
     "ef7294be0808a151090eca0461e41fd449390ad3153358ba374ed80007569e76",
+    "6d4998e2519baa2b14c131508b36c2eb4309e86e06a36bd5c16656dd80b4c2c4",
+    "59f808591ef2907a2e3f9a7352afb6eee5e3c76469958f6e6ac6e7e035815e6d",
 ];
 
 var input = document.getElementById("answer");
